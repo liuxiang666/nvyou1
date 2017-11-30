3283e8e53fdbf85fda88f17f57a2bbe3d93093e7
liuxiang <994828053@qq.com>
2017-11-30 09:47:12 +0800

    22222

--- src/nvyou1/NvYou.java
+++ src/nvyou1/NvYou.java
@@ -4,6 +4,9 @@ public class NvYou {
 
 	public static void main(String[] args) {
 		System.out.println("nvyoulaile");
+		System.out.println("nvyoulaile");
+		System.out.println("nvyoulaile");
+		System.out.println("nvyoulaile");
 
 	}
 
